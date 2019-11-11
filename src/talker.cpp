cfae2e35ff3377c3ecadf3bbf63c467f2ac92b7c
jingCGM <jliang92@terpmail.umd.edu>
2019-11-11 13:55:03 -0500

    cpplint test src test cpplint in src fixed issues according to cpplint run total cpplint in beginner_tutorial folder save cpplint messages in cpplint.txt

--- src/talker.cpp
+++ src/talker.cpp
@@ -4,11 +4,12 @@
  * @brief      The file defined publisher, which publish "chatter" topic
  * @license    This project is released under the BSD-3-Clause License.
  */
-#include <cstdlib>
-#include <sstream>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <tf/transform_broadcaster.h>
+#include <cstdlib>
+#include <sstream>
+
 
 /**
  * @brief main function of subscriber, which publish "chatter" topic
@@ -22,23 +23,23 @@ int main(int argc, char **argv) {
 
   tf::TransformBroadcaster broadcaster;
   tf::Transform transform;
-  transform.setOrigin(tf::Vector3(1,0,0));
+  transform.setOrigin(tf::Vector3(1, 0, 0));
   tf::Quaternion quaternionValue;
   quaternionValue.setRPY(0, 0, 1.57);
   transform.setRotation(quaternionValue);
-
 
   ros::NodeHandle n;
 
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
   int looprate = 0;
-  
+
   if (argc != 2) {
     ROS_WARN_STREAM("default loop rate is 10");
     looprate = 10;
+  } else {
+    looprate = atoi(argv[1]);
   }
-  else looprate = atoi(argv[1]);
 
   if (looprate < 1) looprate = 1;
   else if (looprate > 20) looprate = 20;
@@ -46,15 +47,14 @@ int main(int argc, char **argv) {
   ROS_DEBUG_STREAM("rate: " << looprate);
   ros::Rate loop_rate(looprate);
 
-  
-
   while (ros::ok()) {
     std_msgs::String msg;
     std::stringstream ss;
     ss << "It's a customized message";
     msg.data = ss.str();
 
-    broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "talk"));
+    broadcaster.sendTransform(tf::StampedTransform(transform,
+      ros::Time::now(), "world", "talk"));
 
     chatter_pub.publish(msg);
 
