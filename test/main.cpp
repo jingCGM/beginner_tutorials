c6100167b5153728ad964767f2e28406b7be865a
jingCGM <jliang92@terpmail.umd.edu>
2019-11-11 03:37:20 -0500

    Added content of test files
    main.cpp: added doxygen comments and also ros test headers
    test.cpp: added two test functions one is for frame testing and the other is testing published message

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,21 @@
+/**Copyright (c) 2019 Jing Liang
+ * @file       main.cpp
+ * @date       11/10/2019
+ * @brief      This file is the main file of rostest
+ * @license    This project is released under the BSD-3-Clause License.
+ */
 #include <gtest/gtest.h>
+#include <ros/ros.h>
 
+
+/**
+ * @brief  main function for rostest
+ * @param  argc int
+ * @param  argv arguments value
+ * @return 0
+ */
 int main(int argc, char** argv) {
+  ros::init(argc, argv, "rosTest");
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,68 @@
+/**Copyright (c) 2019 Jing Liang
+ * @file       test.cpp
+ * @date       11/10/2019
+ * @brief      This file is the test file of rostest
+ * @license    This project is released under the BSD-3-Clause License.
+ */
 #include <gtest/gtest.h>
+#include <tf/transform_listener.h>
+#include <std_msgs/String.h>
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+// global variable which is used to store subscribed value 
+static std::string message;
+
+
+/**
+ * @brief Checks if tf frames are right
+ */
+TEST(testTF, should_pass) {
+  ros::NodeHandle nh;
+  tf::TransformListener listener;
+
+  tf::StampedTransform transform;
+
+  bool test = false;
+  bool groundtruth = true;
+
+  tf::Quaternion quaternionValue;
+  quaternionValue.setRPY(0, 0, 1.57);
+
+  while (!test) {
+    try{
+      listener.lookupTransform("/world", "/talk", ros::Time(0), transform);
+      if ( transform.getOrigin() == tf::Vector3(1,0,0) && transform.getRotation() == quaternionValue) {
+	  	test = true;
+	  }
+    }
+    catch (tf::TransformException ex){
+      ROS_INFO("%s",ex.what());
+    }
+  }
+
+  EXPECT_EQ(groundtruth, test);
+}
+
+
+/**
+ * @brief call back function check message when received it
+ * @param message of chatter
+ */
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
+  message = msg->data;
+}
+
+
+/**
+ * @brief Checks if publish message is right
+ */
+TEST(testPublish, should_pass) {
+  ros::NodeHandle nh1;
+  
+  ros::Subscriber sub = nh1.subscribe("chatter", 1000, chatterCallback);
+
+  std::string groundtruth = "It's a customized message";
+
+  while (message == "")  ros::spinOnce();
+
+  EXPECT_EQ(groundtruth, message);
 }
